d92bf1d4e06b7d4e0256702453612594e5b917cd
thenightsuprime007 <carlos.flores.andres21@gmail.com>
2024-08-27 22:49:11 -0500

    auth works!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"auth",component:AuthComponent}
+];
